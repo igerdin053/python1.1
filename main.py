97e4e24a216d219ebd74587a7663de61b4c7a629
igerdin053 <igerdin053@g.rwu.edu>
2021-10-29 11:40:33 -0400

    strings1

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+import sys
+
+def sortedStringList():
+    print("\nArguments passed:", end=" ")
+    n = len(sys.argv)
+    strList = []
+    for i in range(1, n):
+        strList.append(sys.argv[i])
+    strList.sort()
+    print(strList)
+
+def reverseString():
+    n = len(sys.argv)
+    argvString = ''
+    for i in range(1, n):
+        argvString += sys.argv[i] + " "
+    sentenceWords = argvString.split(" ")
+    reverse = ' '.join(reversed(sentenceWords))
+    print(reverse)
 
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
-    print_hi('Professor! here is programming assignment 4.')
+    print_hi('Professor! here is programming assignment 6.')
+    sortedStringList()
+    reverseString()
 
 
 
