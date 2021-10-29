1ad75dae80704c4d1446c74a958e1a6e01423f1d
igerdin053 <igerdin053@g.rwu.edu>
2021-10-29 12:29:17 -0400

    Strings3

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import re
+
+def method1(str):
+    sentence = str.split()
+    for i in range(len(sentence)):
+        print(sentence[i])
+
+def method2(str):
+    new = re.findall(r'\S+', str)
+    for i in range(len(new)):
+        print(new[i])
 
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
-    print_hi('Professor! here is programming assignment 4.')
+    print_hi('Professor! here is programming assignment 6.')
+    str = str(input("Please enter a string: "))
+    method1(str)
+    method2(str)
 
 
 
