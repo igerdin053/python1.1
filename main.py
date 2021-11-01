e15afb9533f06c44a0bfad5b58c3a05986f09610
igerdin053 <igerdin053@g.rwu.edu>
2021-11-01 16:19:27 -0400

    Regex

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import platform
 import re
 import os
+
 
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
-    print_hi('Professor! here is programming assignment 4.')
+    print_hi('Professor! here is programming assignment 7.')
+    osENV = str(os.environ)
+    platformUNAME = str(platform.uname())#os.uname does not work on windows :(
 
+    userNAME = re.findall(r"'USERNAME': '[a-zA-Z0-9]*'", osENV)
+    systemName = re.findall(r"system='[^']*'", platformUNAME)
+    nodeName = re.findall(r"COMPUTERNAME'[^']*'[^']*'", osENV)
+    print(systemName)
+    print(nodeName)
+    print(userNAME)
+    print(osENV)
 
 
